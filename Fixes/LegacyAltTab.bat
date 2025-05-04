ce2692a2aea34aba593b130dee8080fd9b6988f8
ki8y <wyattdavies22@gmail.com>
2025-05-04 18:24:23 -0400

    Delete Fixes/LegacyAltTab.bat
    wrong directory.. my bad.

--- Fixes/LegacyAltTab.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-color a
-echo Enabling old alt+tab switcher...
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /v "AltTabSettings" /t REG_DWORD /d 1 /f
-taskkill /f /im explorer.exe
-start explorer.exe
-echo Done! Press any key to close this window.
-pause >nul
