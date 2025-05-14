0137ff96de693a2c0b650ac80e58df53d893396b
ki8y <wyattdavies22@gmail.com>
2025-05-13 23:13:36 -0400

    Yeahhh

---
+++ GPU/Nvidia/2_NIP/autoimport.bat
@@ -0,0 +1,7 @@
+@echo off
+color 9
+echo Importing Khorvie's NVIDIA Profile Inspector settings...
+"C:\Kon OS\Display Drivers\NVIDIA\2) NVIDIA Profile Inspector\nvidiaProfileInspector.exe" -load "C:\Kon OS\Display Drivers\NVIDIA\2) NVIDIA Profile Inspector\khorvie.nip"
+echo.
+echo Profile imported successfully. Press any key to close this Window.
+pause >nul
